750a72ed9f0bee255bdf4393761704ec44abcfae
jovan <git.keseljevicjovan@proton.me>
2024-07-23 03:41:07 +0200

    Add dmenu-border patch

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -22,3 +22,6 @@ static unsigned int columns    = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
