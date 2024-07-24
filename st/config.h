33564b74a3c5fdc3ff94a1773382298896d92c4f
jovan <git.keseljevicjovan@proton.me>
2024-07-24 15:19:38 +0200

    Add dwm-alwayscenter patch

--- st/config.h
+++ st/config.h
@@ -162,8 +162,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 140;
+static unsigned int rows = 48;
 
 /*
  * Default colour and shape of the mouse cursor
@@ -218,8 +218,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_K,           kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_J,           kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_plus,        kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_underscore,  kscrolldown,    {.i = -1} },
   { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
