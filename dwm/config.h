02d1b33085061bdffaa4f9503ea70f049a55e6ba
jovan <git.keseljevicjovan@proton.me>
2024-07-24 13:44:36 +0000

    Remove second col_cyan in config.h (dwm)

--- dwm/config.h
+++ dwm/config.h
@@ -14,7 +14,6 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#444444";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
