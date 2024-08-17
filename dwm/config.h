1ff4c3662cc05cb396c117dfbbd50be453778668
jovan <git.keseljevicjovan@proton.me>
2024-08-17 07:20:07 +0200

    Add st-xresources

--- dwm/config.h
+++ dwm/config.h
@@ -32,15 +32,15 @@ static const unsigned int alphas[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-//{ "Gimp",         NULL,     NULL,           0,         1,          0,           0,        -1 },
-//{ "Firefox",      NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st-256color",  NULL,     NULL,           0,         0,          1,           0,        -1 },	
-  { NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+    //{ "Gimp",         NULL,     NULL,           0,         1,          0,           0,        -1 },
+    //{ "Firefox",      NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+    { "St",           NULL,     NULL,           0,         0,          1,           0,        -1 },	
+    { NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
