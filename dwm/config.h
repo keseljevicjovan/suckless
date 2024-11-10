a477a5af3efb4defad8482c8ccfe8eca1439a641
jovan <git.keseljevicjovan@proton.me>
2024-11-10 14:21:27 +0100

    Set gaps to zero

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
