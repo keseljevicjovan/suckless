c379ebb26b0c5c80aa69573be136f5e684db98bb
keseljevicjovan <keseljevicjovan@proton.me>
2026-06-08 17:41:15 +0200

    feat(dwl): set isfloating to 1 for pinentry

--- dwl/config.h
+++ dwl/config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	/* app_id             title       tags mask     isfloating   isterm   noswallow   monitor */
 	{ "foot",             NULL,       0,            0,           1,       1,          -1 },
 	{ "foot-float",       NULL,       0,            1,           1,       0,          -1 },
+	{ "Pinentry-gtk",     NULL,       0,            1,           0,       0,          -1 }, /* Start on ONLY tag "9" */
 	{ "Gimp_EXAMPLE",     NULL,       0,            1,           0,       0,          -1 }, /* Start on currently visible tags floating, not tiled */
 	{ "firefox_EXAMPLE",  NULL,       1 << 8,       0,           0,       0,          -1 }, /* Start on ONLY tag "9" */
 };
