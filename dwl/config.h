fa869c8d34c2081b63d751edc550c0d344998128
keseljevicjovan <keseljevicjovan@proton.me>
2026-08-26 14:43:25 +0200

    fix(dwl): re-enable fullscreen layout keybinding

--- dwl/config.h
+++ dwl/config.h
@@ -202,7 +202,7 @@ static const Key keys[] = {
   { MODKEY,                    XKB_KEY_g,                     togglegaps,       {0} },
 	{ MODKEY,                    XKB_KEY_q,                     killclient,       {0} },
 	{ MODKEY,                    XKB_KEY_t,                     setlayout,        {.v = &layouts[0]} },
-//{ MODKEY,                    XKB_KEY_f,                     setlayout,        {.v = &layouts[1]} },
+  { MODKEY,                    XKB_KEY_f,                     setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                    XKB_KEY_m,                     setlayout,        {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,                 setlayout,        {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_v,                     togglefloating,   {0} },
